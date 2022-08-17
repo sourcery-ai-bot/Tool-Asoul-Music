ed71635686912170ab55614909508db1369585e6
sudoskys <coldlando@hotmail.com>
2022-08-17 19:08:05 +0800

    feat:add

---
+++ Runner/Bot.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+# @Time    : 8/17/22 6:00 PM
+# @FileName: Bot.py
+# @Software: PyCharm
+# @Github    ：sudoskys
+import os
+import shutil
+import time
+
+import joblib
+
+from Runner.DataParse import biliParse
+from Runner.EventLib import Tool
+from Runner.Network.Uploader import Upload
+from main import pushService
+
+
+class CallingCounter(object):
+    def __init__(self):
+        pass
+
+    def get(self, key):
+        key = str(key) + time.strftime("%Y-%m-%d-%H", time.localtime())
+        if not hasattr(self, 'count'):
+            self.count = dict()
+        if key in self.count:
+            self.count[key] += 1
+        else:
+            self.count[key] = 0
+        return self.count[key]
+
+
+class ClinetBot:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def life():
+        try:
+            if joblib.load('life.pkl') == "on":
+                return True
+            else:
+                return False
+        except Exception as e:
+            print("Wrong:life.pkl do not exist" + str(e))
+            return False
+
+    def run(self, config):
+        if config.ClientBot.statu:
+            import telebot
+            import joblib
+            bot = telebot.TeleBot(config.botToken)
+            count = CallingCounter()
+            joblib.dump("on", 'life.pkl')
+
+            @bot.message_handler(commands=['start'])
+            def send_welcome(message):
+                bot.reply_to(message, "发送链接，我会发送回音频")
+
+            @bot.message_handler(commands=['about'])
+            def send_about(message):
+                bot.reply_to(message, "https://github.com/sudoskys/Tool-Asoul-Music")
+
+            @bot.message_handler(content_types=['text'])
+            def send_about(message):
+                userID = message.from_user.id
+                if str(userID) == config.ClientBot.owner:
+                    try:
+                        chat_id = message.chat.id
+                        command = message.text
+                        if command == "off":
+                            joblib.dump("off", 'life.pkl')
+                            bot.reply_to(message, 'success！')
+                        if command == "on":
+                            joblib.dump("on", 'life.pkl')
+                            bot.reply_to(message, 'success！')
+                    except Exception as e:
+                        bot.reply_to(message, "Wrong:" + str(e))
+
+            @bot.message_handler(content_types=['text'])
+            def replay(message, items=None):
+                userID = message.from_user.id
+                Name = message.from_user.first_name
+                command = message.text
+                ids, tp = biliParse().get_bili_id(command)
+                if tp == 0:
+                    if count.get(userID) < 30:
+                        if ClinetBot.life():
+                            rssBvidItem = [ids]
+                            if items:
+                                for k, v in items.items():
+                                    rssBvidItem.append(biliParse().get_bili_id(str(v))[0])
+                            try:
+                                if not len(rssBvidItem) == 0:
+                                    Upload(config.desc).deal_audio_list(userID, rssBvidItem, '/music', pushService, local=False)
+                                else:
+                                    print("RSS No New Data")
+                            except BaseException as arg:
+                                try:
+                                    pushService.sendMessage(userID,
+                                                            'Failed post ' + str(rssBvidItem) + '\n Exception:' + str(
+                                                                arg))
+                                except BaseException as e:
+                                    print("推送错误")
+                                # WrongGet.append(str(Nowtime) + '\n 任务错误' + str(rssBvidItem) + str(arg))
+                            finally:
+                                pass
+                                # shutil.rmtree(os.getcwd() + '/music/', ignore_errors=False, onerror=None)
+
+                        else:
+                            Tool().console.print("Bot已经关闭", style='blue')
+
+            bot.infinity_polling()
